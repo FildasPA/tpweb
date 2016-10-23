405b18eb97d0cdc604ff2d2eff945eb1a1b0d936
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-10-23 15:35:30 +0200

    [php/css] split index -> disp users list & update login form css

--- index.php
+++ index.php
@@ -1,64 +1,34 @@
 <!doctype html>
 <html lang="fr">
 <head>
-	<title>Liste des utilisateurs</title>
+	<title>Index - Liste des utilisateurs</title>
 	<meta charset="utf-8" />
 	<meta name="author" content="Delvaux Julien, id:1500182, Boge Julien, id:1502198"/>
-	<meta name="description" content="Liste des utilisateurs enregistrés" />
+	<meta name="description" content="Index du site / Liste des utilisateurs enregistrés" />
   <link rel="stylesheet" href="css/main.css">
 </head>
 	<body>
 
-		<form name="login" method="POST" action="connexion.php" enctype="multipart/form-data">
-			<label><b>Se connecter : </b></label><br><br>
-			<input name="pseudo"    type="text/html" placeholder="pseudo">    <br>
-			<input name="password" type="password" placeholder="Mot de passe"><br><br>
-			<input class="button" type="submit" name="valider" value="Connexion">
-		</form>
+		<div id="main-container">
+			<form id="form-login" name="login" method="POST" action="connexion.php" enctype="multipart/form-data">
+				<label id="main-label">Se connecter</label>
+				<div id="login-form-element">
+					<label>Pseudo</label>
+					<input name="login" type="text" placeholder="Pseudo">
+				</div>
+				<div id="password-form-element">
+					<label>Mot de passe</label>
+					<input name="password" type="password" placeholder="Password">
+				</div>
+				<div id="submit-element">
+					<input id="submit-button" class="button" type="submit" name="valider" value="Connexion">
+				</div>
+			</form>
+
+			<?php include("php/display_users_list.php"); ?>
+			<?php include("includes/bottom.php");	?>
+		</div>
 
 
-		<?php
-			include_once("php/connect_db.php");
-			$conn = connect_db();
-			if(!$conn) {
-				echo "<p style='color:red;'>Impossible de se connecter à la bdd</p>";
-				return;
-			}
-			include_once("php/query.php");
-			display_users($conn);
-			$conn = "";
-
-			// Afficher les utilisateurs
-			function display_users($conn)
-			{
-				// Obtenir la liste des utilisateurs
-				$users = query($conn,"SELECT * FROM personnes;");
-				if(!$users) {
-					echo "<p>La requête demandant la liste des utilisateurs a échoué</p>";
-					return;
-				}
-				// Afficher la liste
-				echo "<br/>";
-				echo "<br/>";
-				echo "<h4>Liste des utilisateurs inscrits:</h4>";
-				echo "<br/>";
-				echo "<table>";
-				echo "<tr>";
-				echo "<th>Nom</th>";
-				echo "<th>Prénom</th>";
-				echo "</tr>";
-				while($user = $users->fetch(PDO::FETCH_ASSOC)) {
-					echo "<tr class='user' title=\"Voir le profil\" onclick=\"window.document.location='view_profile.php?id=" . $user['id'] . "';\">";
-					echo "<td>" . $user['nom']    . "</td>";
-					echo "<td>" . $user['prenom'] . "</td>";
-					echo "</tr>";
-				}
-				echo "</table>";
-			}
-
-			// javascript page précédente: document.referrer
-		?>
-
-		<?php include("includes/bottom.php");	?>
 	</body>
 </html>
---
+++ php/display_users_list.php
@@ -0,0 +1,65 @@
+<?php
+
+//==============================================================================
+//
+// ■ Display users list
+// -- Objet : Affiche la liste des utilisateurs enregistrés dans la bdd
+// -- Par : Julien Delvaux & Julien Boge
+// -- Dernière modification : 23.10.16
+//
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// * Afficher la liste des utilisateurs
+//------------------------------------------------------------------------------
+function display_users()
+{
+	// Connexion bdd
+	include_once("php/connect_db.php");
+	$conn = connect_db();
+	if(!$conn) {
+		echo "<p style='color:red;'>Impossible de se connecter à la bdd</p>";
+		return;
+	}
+
+	try {
+		// Obtenir la liste des utilisateurs (100 max, à partir du début)
+		$sql = "SELECT id,login
+	          FROM personnes
+	          LIMIT 100";
+		$users = $conn->prepare($sql);
+		$users->execute();
+		if(!$users) {
+			echo "<p>La requête demandant la liste des utilisateurs a échoué</p>";
+			return;
+		}
+
+		// Afficher la liste des utilisateurs
+		echo "<h4>Liste des utilisateurs inscrits:</h4>";
+		if($users->rowCount() <= 0) {
+			echo "<p>Aucun utilisateur enregistré</p>";
+			return;
+		}
+		echo "<table>";
+		echo "<tr>";
+		// echo "<th>Nom</th>";
+		// echo "<th>Prénom</th>";
+		echo "<th>Pseudo</th>";
+		echo "</tr>";
+		while($user = $users->fetch(PDO::FETCH_ASSOC)) {
+			echo "<tr class='user' title=\"Voir le profil\" onclick=\"window.document.location='view_profile.php?id=" . $user['id'] . "	';\">";
+			echo "<td>" . $user['login']    . "</td>";
+			// echo "<td>" . $user['nom']    . "</td>";
+			// echo "<td>" . $user['prenom'] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}	catch(Exception $e) {
+		echo "<br/>" . $e->getMessage() . "<br/>";
+		return;
+	}
+}
+
+display_users();
+
+?>
