9460cffbfab707f028c62fac19b2d4a54256460a
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-10-23 13:46:33 +0200

    [php] (register) bind queries parameters ; fixed avatar copy

--- php/register.php
+++ php/register.php
@@ -1,5 +1,14 @@
 <?php
 
+//==============================================================================
+//
+// ■ Register
+// -- Objet : Traitement des données du formulaire d'inscription
+// -- Par : Julien Delvaux & Julien Boge
+// -- Dernière modification : 23.10.16
+//==============================================================================
+
+
 // include("php/display_globals.php"); // affiche le contenu des variables globales
 
 //------------------------------------------------------------------------------
@@ -15,21 +24,7 @@ function test_input($data)
 }
 
 //------------------------------------------------------------------------------
-// * Copie l'avatar vers le serveur
-//------------------------------------------------------------------------------
-function copy_avatar($url,$dest_file)
-{
-	$dest_dir  = dirname(__FILE__) . "/pictures/";
-	$dest_file = $dest_dir . $dest_file;
-	try {
-		copy($url,$dest_file);
-	} catch(Exception $e) {
-		echo "<p style='color:red;'>L'image n'a pas pu être ajoutée...</p>";
-	}
-}
-
-//------------------------------------------------------------------------------
-// * Vérifie si les informations sont correctes
+// * Vérifie si les informations sont correctes (vide & longueur)
 //------------------------------------------------------------------------------
 function check_user_info($user,$avatar_url)
 {
@@ -46,34 +41,62 @@ function check_user_info($user,$avatar_url)
 }
 
 //------------------------------------------------------------------------------
-// * Vérifie si un utilisateur avec le même nom et le même prénom existe déjà
-// Renvoie vrai si la requête échoue
+// * Copie l'avatar sur le serveur
 //------------------------------------------------------------------------------
-function user_already_exists($conn,$user)
+function copy_avatar($url,$dest_file)
 {
-	$stmt = query($conn,"SELECT id FROM personnes WHERE login='$user[login]'");
-	if(!$stmt || $stmt->rowCount() > 0) {
+	$dest_dir  = dirname($_SERVER['SCRIPT_FILENAME']) . "/pictures/";
+	$dest_file = $dest_dir . $dest_file;
+	if(copy($url,$dest_file)) {
+		return true;
+	}	else {
+		echo "<p style='color:red;'>L'image n'a pas pu être ajoutée...</p>";
+		return false;
+	}
+}
+
+//------------------------------------------------------------------------------
+// * Vérifie si le login est déjà pris
+//------------------------------------------------------------------------------
+function user_login_used($conn,$login)
+{
+	try {
+		$user_exists = $conn->prepare("SELECT login
+	                                 FROM personnes
+	                                 WHERE login = :login");
+		$user_exists->bindParam(':login',$user['login'],PDO::PARAM_STR,15);
+		$user_exists->execute();
+		if(!$user_exists || $user_exists->rowCount() > 0) {
+			echo "<p style='color:red;'>Un autre utilisateur avec le même pseudo existe déjà!</p>";
+			return true;
+		}
+		return false;
+	}	catch(Exception $e) {
+		echo "<br/>" . $e->getMessage() . "<br/>";
 		return true;
 	}
-	return false;
 }
+
 
 //------------------------------------------------------------------------------
 // * Ajoute l'utilisateur à la base de données
 //------------------------------------------------------------------------------
 function insert_user($conn,$user)
 {
-	if(user_already_exists($conn,$user)) {
-		echo "<p style='color:red;'>Un autre utilisateur avec le même pseudo existe déjà!</p>";
+	try { // Insère l'utilisateur
+		$insert = $conn->prepare("INSERT INTO personnes (nom,prenom,avatar,login,password)
+	                            VALUES (:name,:firstname,:avatar,:login,:password)");
+		$insert->bindParam(':name',      $user['name'],      PDO::PARAM_STR,15);
+		$insert->bindParam(':firstname', $user['firstname'], PDO::PARAM_STR,15);
+		$insert->bindParam(':avatar',    $user['avatar'],    PDO::PARAM_STR,30);
+		$insert->bindParam(':login',     $user['login'],     PDO::PARAM_STR,15);
+		$insert->bindParam(':password',  $user['password'],  PDO::PARAM_STR,32);
+		$insert->execute();
+		return true;
+	}	catch(Exception $e) {
+		echo "<br/>" . $e->getMessage() . "<br/>";
 		return false;
 	}
-	$insert = query($conn,"INSERT INTO personnes (nom,prenom,avatar,login,password)
-	                VALUES ('$user[name]','$user[firstname]','$user[avatar]','$user[login]','$user[password]')");
-	if(!$insert) {
-		echo "<p style='color:red;'>Erreur: l'utilisateur n'a pas pu être ajouté!</p>";
-		return false;
-	}
-	return true;
 }
 
 //------------------------------------------------------------------------------
@@ -83,21 +106,21 @@ function insert_user($conn,$user)
 //------------------------------------------------------------------------------
 function register($user,$avatar_url)
 {
+
+	// Vérifie les infos
 	if(!check_user_info($user,$avatar_url)) return false;
+
+	// Connexion bdd
 	include_once("php/connect_db.php");
 	$conn = connect_db();
 	if(!$conn) {
 		echo "<p style='color:red;'>Impossible de se connecter à la bdd</p>";
 		return false;
 	}
-	include_once("php/query.php");
-	if(insert_user($conn,$user)) {
-		copy_avatar($avatar_url,$user['avatar']);
-		$conn = "";
-		return true;
-	}
-	$conn = "";
-	return false;
+
+	return (!user_login_used($conn,$user['login']) &&
+	        insert_user($conn,$user,$avatar_url) &&
+		      copy_avatar($avatar_url,$user['avatar']));
 }
 
 //------------------------------------------------------------------------------
@@ -122,7 +145,7 @@ if($is_info_set) {
 	// URL de l'image (serveur & upload)
 	$avatar_url     = $_FILES['avatar']['tmp_name'];
 	$avatar_name    = $_FILES['avatar']['name'];
-	$image_type     = substr($avatar_name,strpos($avatar_name,"."));
+	$image_type     = substr($avatar_name,strrpos($avatar_name,"."));
 	$user['avatar'] = 'avatar_' . $user['login'] . $image_type;
 
 	// Inscription
@@ -131,13 +154,14 @@ if($is_info_set) {
 		echo "<p style='color:green;'>Utilisateur enregistré!</p>";
 		echo "<p>Redirection vers l'<a href='index.php'>index</a>...</p>";
 		header('refresh:5;url=index.php');
-		exit();
+		exit;
 	}
 
 	unset($user['password']);
 	unset($avatar_name);
 	unset($image_type);
 	unset($avatar_url);
+
 }
 
 
