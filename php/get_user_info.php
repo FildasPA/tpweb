f88b401851356ebe200771629132d1f74f142c46
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-10-23 16:24:20 +0200

    [php/css] view public profile (+ get user info)

---
+++ php/get_user_info.php
@@ -0,0 +1,40 @@
+<?php
+
+//==============================================================================
+//
+// ■ Get user profile
+// -- Objet : Renvoie les informations de l'utilisateur correspondant à l'id envoyée
+// -- Par : Julien Delvaux & Julien Boge
+// -- Dernière modification : 23.10.16
+//
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// * Retourne l'utilisateur correspondant à l'id envoyée en paramètre
+//------------------------------------------------------------------------------
+function get_user_info($id) {
+
+	// Connexion bdd
+	include_once("php/connect_db.php");
+	$conn = connect_db();
+	if(!$conn) {
+		echo "<p style='color:red;'>Impossible de se connecter à la bdd</p>";
+		return NULL;
+	}
+
+	try {
+		$sql = "SELECT nom,prenom,avatar,login
+		        FROM personnes
+		        WHERE id = :id";
+		$user = $conn->prepare($sql);
+		$user->bindParam(':id',$id,PDO::PARAM_INT);
+		$user->execute();
+		if(!$user || $user->rowCount() <= 0) return NULL;
+		return $user->fetch(PDO::FETCH_ASSOC);
+	}	catch(Exception $e) {
+		echo "<br/>" . $e->getMessage() . "<br/>";
+		return NULL;
+	}
+}
+
+?>
--- view_profile.php
+++ view_profile.php
@@ -9,63 +9,41 @@
 </head>
 
 	<body>
+		<div id="main-container">
+
 		<?php
-			include_once("php/connect_db.php");
-			$conn = connect_db();
-			if(!$conn) {
-				echo "<p style='color:red;'>Impossible de se connecter à la bdd</p>";
-				return;
-			}
-			include_once("php/query.php");
 
-
-			//------------------------------------------------------------------------------
-			// * Retourne l'utilisateur correspondant à l'id envoyée en paramètre
-			//------------------------------------------------------------------------------
-			function get_user($conn,$id) {
-				$user = query($conn,"SELECT * FROM personnes WHERE id = $id;");
-				if(!$user) {
-					echo "<p>Aucun utilisateur avec l'id $id n'a été trouvé.</p>";
-					return;
-				}
-				$user = $user->fetch(PDO::FETCH_ASSOC);
-				return $user;
-			}
-
-			//------------------------------------------------------------------------------
-			// * Formulaire permettant de modifier un utilisateur envoyé en paramètre
-			//------------------------------------------------------------------------------
-			function modify_user($conn,$user) {
-				echo "<br/>";
-				echo "<br/>";
-				echo "<h4>Profil de " . $user['login'] . ":</h4>";
-				echo "<br/>";
-				echo "<table>";
-				echo "<tr>";
-				echo "<th>Id</th>";
-				echo "<th>Login</th>";
-				echo "<th>Nom</th>";
-				echo "<th>Prénom</th>";
-				echo "<th>Adresse avatar</th>";
-				echo "</tr>";
-				echo "<tr>";
-				echo "<td>" . $user['id']     . "</td>";
-				echo "<td>" . $user['login'] . "</td>";
-				echo "<td> ". $user['nom'] . "</td>";
-				echo "<td>" . $user['prenom'] . "</td>";
-				echo "<td style=\"width:200px\">" . $user['avatar'] . "</td>";
-				echo "</tr>";
-				echo "</table>";
-			}
-
-			$id   = $_REQUEST['id'];
-			$user = get_user($conn,$id);
-			modify_user($conn,$user);
-
-			$conn = "";
+		include("php/get_user_info.php");
+		$id   = (int) $_REQUEST['id'];
+		$user = get_user_info($id);
+		if(!$user) { // Si l'utilisateur n'existe pas, affiche l'erreur et le footer
+			echo "<p>L'id $id ne correspond à aucun utilisateur.</p>";
+			include("includes/footer.php");
+			exit;
+		}
 
 		?>
 
-		<?php include("includes/bottom.php");	?>
+		<div id="user-profile">
+			<h3>Profil de <?php echo $user['login']; ?></h3>
+			<div id="user-info-block">
+				<div class="info-element">
+					<div class="info-label">Prénom:</div>
+					<div id="user-firstname"><?php echo $user['prenom']; ?></div>
+				</div>
+				<div class="info-element">
+					<div class="info-label">Nom:</div>
+					<div id="user-name"><?php echo $user['nom']; ?></div>
+				</div>
+				<div class="info-element">
+					<div class="info-label">Avatar:</div>
+					<img id="user-avatar-image" src="pictures/<?php echo $user['avatar']; ?>">
+				</div>
+			</div>
+		</div>
+
+		<?php include("includes/footer.php");	?>
+
+		</div>
 	</body>
 </html>
